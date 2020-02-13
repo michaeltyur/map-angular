9211f21c351fbf120d4285bed365a0b502504e70
Michael Tyurikov <mtyurikov@dxc.com>
2020-02-13 13:06:40 +0200

    htnl

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {SidebarModule} from 'primeng/sidebar';
+import { AgmCoreModule } from '@agm/core';
  
 
 @NgModule({
@@ -14,7 +15,14 @@ import {SidebarModule} from 'primeng/sidebar';
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    SidebarModule
+    SidebarModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyCiX1imd32VwCLDtX_PpSRee7HIeYI3_3g'
+      /* apiKey is required, unless you are a 
+      premium customer, in which case you can 
+      use clientId 
+      */
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
