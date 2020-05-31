dee25f0562b7dcbbffe019fd2b72305f42fdd2e4
Bob Bobov <bobyy@yandex.ru>
2020-05-31 13:56:50 +0300

    map marker fix

--- src/app/ui-components/google-map/google-map.component.ts
+++ src/app/ui-components/google-map/google-map.component.ts
@@ -4,9 +4,10 @@ import { ActivatedRoute, ParamMap } from '@angular/router';
 import { filter, switchMap } from 'rxjs/operators';
 import { NbSidebarService } from '@nebular/theme';
 import { SearchService } from 'src/app/shared/services/search.service';
-import { Place } from 'src/app/shared/models/firebase-collection-models';
+import { Place, PlaceImages } from 'src/app/shared/models/firebase-collection-models';
 import { Subscription } from 'rxjs';
 import { DeviceDetectorService } from 'ngx-device-detector';
+import { AspService } from 'src/app/shared/services/asp.service';
 
 @Component({
   selector: 'app-google-map',
@@ -21,12 +22,15 @@ export class GoogleMapComponent implements OnInit, OnDestroy {
   mapType = 'hybrid';
   zoom = 8;
   isMobile: boolean;
+  markerImageSrc:string;
+
   constructor(
     private mapNavigationService: MapNavigationService,
     private route: ActivatedRoute,
     private sidebarService: NbSidebarService,
     private searchService: SearchService,
     private deviceService: DeviceDetectorService,
+    private aspService: AspService
   ) { }
 
   ngOnInit(): void {
@@ -51,30 +55,56 @@ export class GoogleMapComponent implements OnInit, OnDestroy {
 
     this.latitude = +this.route.snapshot.paramMap.get('latitude') ? +this.route.snapshot.paramMap.get('latitude') : this.latitude;
     this.longitude = +this.route.snapshot.paramMap.get('longitude') ? +this.route.snapshot.paramMap.get('longitude') : this.longitude;
-    //this.sidebarService.expand();
 
+    this.getPlaces();
   }
+
   ngOnDestroy() {
     this.subscription.unsubscribe();
   }
 
+  getPlaces(): void {
+    this.subscription.add(this.aspService.getAllPlaces().subscribe((places: Place[]) => {
+      if (places) {
+        this.places = places;
+      }
+    }));
+  }
+
+  getImagesByPlaceID(placeID: number):void {
+    this.aspService.getPlaceImagesByParentID(placeID).subscribe((res: PlaceImages[]) => {
+      if (res && res.length) {
+        this.markerImageSrc = res[0].imagePath;
+      }
+    })
+  }
+
+  markerImageSrcClear():void{
+    this.markerImageSrc = null;
+  }
 
   toggle() {
     this.sidebarService.toggle();
   }
 
-  onMouseOver(infoWindow, $event: MouseEvent) {
+  onMouseOver(infoWindow, $event: MouseEvent, place: Place) {
+    this.getImagesByPlaceID(place.placeID);
     infoWindow.open();
   }
 
   onMouseOut(infoWindow, $event: MouseEvent) {
     infoWindow.close();
+    this.markerImageSrcClear();
   }
 
   onMapMarkerClick(place: Place): void {
     this.sidebarService.collapse();
     this.searchService.placeDetailsEmitter$.emit(place);
     this.sidebarService.expand();
+  }
+
+  imageLoadError(event): void {
+    this.markerImageSrc = null;
   }
 
 }
